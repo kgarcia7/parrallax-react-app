04e4dc3f9a368897b0f4bcec706fa025d7561cc5
kgarcia7 <kgarcia7814@gmail.com>
2024-02-28 14:22:55 -0500

    Update styling

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -2,7 +2,7 @@ import { Parallax } from "react-parallax";
 import Beach from "../img/beachimageone.jpg";
 
 const ImageOne = () => (
-  <Parallax className="image" bgImage={Beach} strength={900}>
+  <Parallax className="image" bgImage={Beach} strength={800}>
     <div className="content">
       <span className="img-txt">
         Lost in the rhythm of the waves, found in the beauty of the shore.
